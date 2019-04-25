f5a046ad685d1e26191a98a1cf0c23512bab754e
Sergei Kuznetcov <SergSmith64@users.noreply.github.com>
2019-04-25 22:32:04 +0400

    commit_02_Empty_Project

---
+++ Task_1_Sort/Task_1_Sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дан массив целых чисел : ");
+            Console.ReadKey();
+        }
+    }
+}
