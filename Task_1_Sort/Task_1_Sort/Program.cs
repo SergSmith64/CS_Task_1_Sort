48981fa921e500f66f76be038cbed3482ca7a63a
SergSmith64 <sergsmith2008@yandex.ru>
2019-04-26 10:53:56 +0400

    commit_03_Task_completed

--- Task_1_Sort/Task_1_Sort/Program.cs
+++ Task_1_Sort/Task_1_Sort/Program.cs
@@ -8,10 +8,47 @@ namespace Task_1_Sort
 {
     class Program
     {
+        // Инициализация массива
+        int[] a = { 73, 45, 88, 13, 55, 49, 11, 82 };
+
+        // ВЫВОД МАССИВА В КОНСОЛЬ
+        public void PrintArray()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + "  ");
+            }
+        }
+
+        // СОРТИРОВКА ПУЗЫРЬКОМ
+        public void bubbleSort()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a [i] > a [j])
+                    {
+                        int t = a[i];
+                        a[i] = a[j];
+                        a[j] = t;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Дан массив целых чисел : ");
+            Console.WriteLine("Дан массив целых чисел из 8 элементов: ");
+
+            Program sort = new Program();
+            sort.PrintArray();
+            sort.bubbleSort();
+            Console.WriteLine();
+            Console.WriteLine("ОТСОРТИРОВАННЫЙ МАССИВ :");
+            sort.PrintArray();
             Console.ReadKey();
+
         }
     }
 }
