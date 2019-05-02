b4b537bd98f264af4fc4bdd16590fbc40a7dbdeb
SergSmith64 <sergsmith2008@yandex.ru>
2019-05-03 02:08:39 +0400

    commit_05_Add_Check_Input_and_Recursion__Task_02

--- Task_1_Sort/Task_1_Sort/Program.cs
+++ Task_1_Sort/Task_1_Sort/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_1_Sort
 {
@@ -10,6 +6,21 @@ namespace Task_1_Sort
     {
         // Инициализация массива
         int[] a = { 73, 45, 88, 13, 55, 49, 11, 82 };
+        int f1;
+
+        // 2 мая
+        // ПУСТОЙ МАССИВ
+        // int[] a = {};
+
+        // ДОБАВИЛ ПРОВЕРКУ НА ПУСТОЙ МАССИВ
+        //if(a.Length == 0) Console.WriteLine("Массив пуст");
+
+        // или такую проверку:
+        // if (a == null || a.Length == 0) 
+        //{ 
+        //Console.WriteLine("Ваш массив пуст!");
+        //}
+
 
         // ВЫВОД МАССИВА В КОНСОЛЬ
         public void PrintArray()
@@ -27,7 +38,7 @@ namespace Task_1_Sort
             {
                 for (int j = i + 1; j < a.Length; j++)
                 {
-                    if (a [i] > a [j])
+                    if (a[i] > a[j])
                     {
                         int t = a[i];
                         a[i] = a[j];
@@ -37,47 +48,98 @@ namespace Task_1_Sort
             }
         }
 
-        public void findElem()
-        { 
-        	// ОПРЕДЕЛЯЕМ ЗАДАННОЕ ЗНАЧЕНИЕ ДЛЯ ПОИСКА ПО МАССИВУ
-        	Console.WriteLine("Введите число = ");
-        	int f1 = Convert.ToInt32(Console.ReadLine());
-        	
-        	for (int i = 0; i < a.Length; i++) 
+        public void checkInput()
+        {
+            // ВВЕДЕМ ЗНАЧЕНИЕ ДЛЯ ПОИСКА ПО МАССИВУ
+            Console.WriteLine("\nВведите число = ");
+
+            // -- старый вариант --
+            // int f1 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            // -- новый вариант --
+            string stroka = Console.ReadLine();
+            // int f1;
+
+            // rezult = true ЕСЛИ ВВЕЛИ ЧИСЛО и false ЕСЛИ НЕ ЧИСЛО
+            bool rezult = int.TryParse(stroka, out f1);
+
+            // ПОТОМ УБРАТЬ__________________________________
+            Console.WriteLine("Значение f1 = " + f1);
+            Console.WriteLine("Значение stroka = " + stroka);
+
+
+            if (rezult)
             {
-            	if (a[i] != f1) 
-            	{ 
-            		//Console.WriteLine($"введенного значения {a[i]} в массиве НЕ НАЙДЕНО");
-            	}
-            	else 
-            	{ 
-            		Console.WriteLine($"введенное значение {a[i]} присутствует в массиве!");
-            		break;
-            	}
-            	if (i == a.Length-1) 
-            	{ 
-            		Console.WriteLine($"введенного значения {f1} в массиве НЕ НАЙДЕНО");
-            	}
+                Console.WriteLine("Значение для поиска ВВЕДЕНО КОРРЕКТНО = " + f1);
+                findElem();
+            }
+            else
+            {
+                Console.WriteLine("НЕКОРРЕКТНЫЙ ВВОД ДАННЫХ");
+                // ПРОБУЮ РЕКУРСИЮ
+                checkInput();
             }
         }
 
+        public void findElem()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                // ПОТОМ УБРАТЬ__________________________________
+                Console.WriteLine("Значение i = " + i);
+                Console.WriteLine("Значение a.Length = " + a.Length);
+                Console.WriteLine("Значение a[] = " + a[i]);
+                Console.WriteLine("Значение f1 = " + f1);
+
+
+                if (a[i] != f1)
+                    {
+                    //Console.WriteLine($"введенного значения {a[i]} в массиве НЕ НАЙДЕНО");
+                }
+                else
+                {
+                    Console.WriteLine($"введенное значение {a[i]} присутствует в массиве!");
+                    break;
+                }
+                if (i == a.Length - 1)
+                {
+                    Console.WriteLine($"введенного значения {f1} в массиве НЕ НАЙДЕНО");
+                }
+            }
+        }
+
+
+
+        // 2. Не обработан случай пустого массива.
+        // Сама функция не выведен ничего в случае пустого массива, так как цикла перебора не будет в принципе:
+        // _______________________ a.Length
+        // Предлагаемое решение:
+        //- универсализировать алгоритм;
+        // Рекомендации.
+        // - разделить логику и визуал (пусть это лишь консольный вывод).
+        // Не завязывать функции с логикой (прим. findElem())и конкретный вывод.
+        // Например, можно возвращать bool;
+
+
         static void Main(string[] args)
         {
-        	Console.WriteLine("ЗАДАНИЕ-1:");
+            Console.WriteLine("ЗАДАНИЕ-1:");
             Console.WriteLine("\nДан массив целых чисел из 8 элементов: ");
 
             Program sort = new Program();
             sort.PrintArray();
             sort.bubbleSort();
 
-            Console.WriteLine("\nОТСОРТИРОВАННЫЙ МАССИВ :");
+            Console.WriteLine("\n\nОТСОРТИРОВАННЫЙ МАССИВ :");
             sort.PrintArray();
 
-            Console.WriteLine();
-            Console.WriteLine($"\nЗАДАНИЕ-2: \nПоиск заданного значения в отсортированном массиве.");
-            Console.WriteLine();
+            Console.WriteLine($"\n\nЗАДАНИЕ-2: \n\nПоиск заданного значения в отсортированном массиве.");
 
-            sort.findElem();
+            sort.checkInput();
+
+            
 
             Console.ReadKey();
 
